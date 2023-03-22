bd7580bb8be524d708e4b37056c81b9741bf1127
RuslanDobrov <ruslandobrov@protone.me>
2023-03-22 13:39:02 +0300

    move project from JDBC to Hibernate

---
+++ src/main/java/todolist/domain/Priority.java
@@ -0,0 +1,5 @@
+package todolist.domain;
+
+public enum Priority {
+    low, medium, high
+}
--- src/main/java/todolist/services/UserService.java
+++ src/main/java/todolist/services/UserService.java
@@ -1,8 +1,6 @@
 package todolist.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 import todolist.domain.User;
 import todolist.services.interfaces.IUserService;
@@ -10,41 +8,28 @@ import todolist.services.interfaces.IUserService;
 @Service
 public class UserService implements IUserService {
 
-    private final JdbcTemplate jdbcTemplate;
-
     @Autowired
-    public UserService(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
+    public UserService() {
+
     }
 
     @Override
     public int createUser(User user) {
-        String query = "INSERT INTO dbo.Users VALUES(" + user.getId() + ",'" + user.getEmail() + "','" + user.getPassword() + "')";
-        return jdbcTemplate.update(query);
+        return 0;
     }
 
     @Override
     public User getUser(long id) {
-        String query = "SELECT * FROM dbo.Users WHERE Users.Id = ?";
-        return jdbcTemplate.queryForObject(query, new BeanPropertyRowMapper<>(User.class), id);
+        return null;
     }
 
     @Override
     public int updateUser(User updateUser, long id) {
-        String query = "UPDATE dbo.Users SET Email = '" + updateUser.getEmail() + "', Password = '" + updateUser.getPassword() +
-                       "' WHERE id =" + id;
-        return jdbcTemplate.update(query);
+        return 0;
     }
 
     @Override
     public int deleteUser(long id) {
-        String query = "DELETE FROM dbo.USERS WHERE id = " + id;
-        return jdbcTemplate.update(query);
-    }
-
-    @Override
-    public void createTableUser() {
-        jdbcTemplate.execute("IF OBJECT_ID('dbo.Users', 'U') IS NOT NULL DROP TABLE dbo.Users");
-        jdbcTemplate.execute("CREATE TABLE Users(Id BIGINT, Email VARCHAR(30), Password VARCHAR(30))");
+        return 0;
     }
 }
